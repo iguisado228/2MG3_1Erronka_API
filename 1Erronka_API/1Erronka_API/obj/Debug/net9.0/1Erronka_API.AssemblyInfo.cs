0066e5066996bac6fc03f3d0d6968af912ab7d6a
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-11-27 13:25:01 +0100

    fluentNhibernate, Nhibernate eta Mysql paketeak instalatuta

--- 1Erronka_API/1Erronka_API/obj/Debug/net9.0/1Erronka_API.AssemblyInfo.cs
+++ 1Erronka_API/1Erronka_API/obj/Debug/net9.0/1Erronka_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1Erronka_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06060f755f298295f1f095d9e2af436bd7ad5c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("1Erronka_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1Erronka_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
